f13bda5a3e6b2b220e39d080825cd640ddc0c71b
reniipp <perezportillorenata@gmail.com>
2026-05-04 13:58:58 -0300

    update

--- java.js
+++ java.js
@@ -29,7 +29,7 @@ setTimeout(() => {
       overlay.classList.add("visible");
     }, 600);
   }
-}, 1500);
+}, 1580);
 
 // Act I → Act II
 document.getElementById("btnEnviar1").addEventListener("click", () => {
